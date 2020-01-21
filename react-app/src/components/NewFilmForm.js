6e74959b51a0d51be179daccf1bf290c95b92c25
andrew-lewell <andrew.lewell@gmail.com>
2020-01-21 17:35:13 +0000

    post film and frame

---
+++ react-app/src/components/NewFilmForm.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import API from "../adapters/API";
+
+const NewFilmForm = ({ onSuccess }) => {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: ""
+  });
+
+  const handleChange = event => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const blankArrayGenerator = (xdim, ydim, color) =>
+    [...Array(ydim)].map(() => Array(xdim).fill(color));
+
+  //   const defaultFrame = JSON.parse(blankArrayGenerator(40, 25, `FFF`));
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    API.postFilm(formData)
+      .then(film => {
+        onSuccess(film);
+        API.postFrame(
+          film.id,
+          JSON.stringify(blankArrayGenerator(40, 25, "#FFF")),
+          0
+        );
+        return film;
+      })
+      .then(console.log);
+  };
+
+  return (
+    <form onChange={handleChange} onSubmit={handleSubmit}>
+      <input
+        type="title"
+        name="title"
+        placeholder="Film title"
+        value={formData.title}
+      />
+      <textarea
+        name="description"
+        placeholder="Film description"
+        value={formData.description}
+      />
+      <input type="submit" value="Create film" />
+    </form>
+  );
+};
+
+export default NewFilmForm;
--- react-app/src/containers/Animator.js
+++ react-app/src/containers/Animator.js
@@ -1,71 +1,104 @@
-import React, {useState,  useEffect, useRef} from 'react';
-import '../css/Animator.css';
-import demoFilm from '../components/BaseFrames.js'
-import FrameEditor from './FrameEditor';
-import FrameSelector from './FrameSelector';
+import React, { useState, useEffect, useRef } from "react";
+import "../css/Animator.css";
+import demoFilm from "../components/DemoFilms.js";
+import FrameEditor from "./FrameEditor";
+import FrameSelector from "./FrameSelector";
 
 const Animator = () => {
-  const[pixelDimensions] = useState([40, 25])
-  const[frames, setFrames] = useState(demoFilm);
-  const[frameNum, setframeNum] = useState(0);
-  const[previewMode, setPreviewMode] = useState(false)
-  const[previewRate, setPreviewRate] = useState('1')
+  const [pixelDimensions] = useState([40, 25]);
+  const [frames, setFrames] = useState(demoFilm);
+  const [frameNum, setframeNum] = useState(0);
+  const [previewMode, setPreviewMode] = useState(false);
+  const [previewRate, setPreviewRate] = useState("1");
 
-  useInterval(() => {
-    changeFrame('next');
-  }, previewMode ? 1000/previewRate : null);
+  useInterval(
+    () => {
+      changeFrame("next");
+    },
+    previewMode ? 1000 / previewRate : null
+  );
 
   const changeFrame = i => {
-    if (i === 'next') setframeNum(frameNum===frames.length-1 ? 0 : frameNum + 1)
-    else if (i === 'prev') setframeNum(frameNum === 0 ? frames.length-1 : frameNum - 1);
-    else setframeNum(i)
-  }
+    if (i === "next")
+      setframeNum(frameNum === frames.length - 1 ? 0 : frameNum + 1);
+    else if (i === "prev")
+      setframeNum(frameNum === 0 ? frames.length - 1 : frameNum - 1);
+    else setframeNum(i);
+  };
 
   const deleteFrame = i => {
     if (frames.length > 1) {
-      if (i <= frameNum) {frameNum !== 0 && setframeNum(frameNum-1)}
-      setFrames(frames.filter((f, index) => index !== i))
+      if (i <= frameNum) {
+        frameNum !== 0 && setframeNum(frameNum - 1);
+      }
+      setFrames(frames.filter((f, index) => index !== i));
     }
-  }
+  };
 
   const duplicateFrame = i => {
-    let newFrames = frames.slice()
-    newFrames.splice(i+1, 0, frames[i])
-    setFrames(newFrames)
-    setframeNum(i+1)
-  }
+    let newFrames = frames.slice();
+    newFrames.splice(i + 1, 0, frames[i]);
+    setFrames(newFrames);
+    setframeNum(i + 1);
+  };
 
   const togglePreviewMode = () => {
-    if (previewMode) {setPreviewMode(false)}
-    else if (!previewMode) {setPreviewMode(true)}
-  }
+    if (previewMode) {
+      setPreviewMode(false);
+    } else if (!previewMode) {
+      setPreviewMode(true);
+    }
+  };
 
   const addBlankFrame = () => {
-    const blankArrayGenerator = (xdim, ydim, color) => [...Array(ydim)].map(() => Array(xdim).fill(color));
-    setFrames([...frames, blankArrayGenerator(pixelDimensions[0], pixelDimensions[1], '#FFF')])
-    setframeNum(frames.length)
-  }
+    const blankArrayGenerator = (xdim, ydim, color) =>
+      [...Array(ydim)].map(() => Array(xdim).fill(color));
+    setFrames([
+      ...frames,
+      blankArrayGenerator(pixelDimensions[0], pixelDimensions[1], "#FFF")
+    ]);
+    setframeNum(frames.length);
+  };
 
-  return(
-    <div className='AnimatorDiv'>
-       <FrameEditor 
+  return (
+    <div className="AnimatorDiv">
+      <FrameEditor
         selectedFrame={frames[frameNum]}
-        frameUp={frames.length > 1 ? (frameNum===frames.length-1 ? frames[0] : frames[frameNum+1]) : null} 
-        frameDown={frames.length > 1 ? (frameNum === 0 ? frames[frames.length-1] : frames[frameNum-1]) : null} 
-        updateFrame={edit => setFrames(frames.map((val, index) => index === frameNum ? edit : val ))}
-        />
-       <FrameSelector 
+        frameUp={
+          frames.length > 1
+            ? frameNum === frames.length - 1
+              ? frames[0]
+              : frames[frameNum + 1]
+            : null
+        }
+        frameDown={
+          frames.length > 1
+            ? frameNum === 0
+              ? frames[frames.length - 1]
+              : frames[frameNum - 1]
+            : null
+        }
+        updateFrame={edit =>
+          setFrames(
+            frames.map((val, index) => (index === frameNum ? edit : val))
+          )
+        }
+      />
+      <FrameSelector
         deleteFrame={i => deleteFrame(i)}
         duplicateFrame={i => duplicateFrame(i)}
-        previewMode={previewMode} togglePreviewMode={() => togglePreviewMode()}
-        previewRate={previewRate} changePreviewRate={val => setPreviewRate(val)}
+        previewMode={previewMode}
+        togglePreviewMode={() => togglePreviewMode()}
+        previewRate={previewRate}
+        changePreviewRate={val => setPreviewRate(val)}
         addBlankFrame={() => addBlankFrame()}
-        frames={frames} frameNum={frameNum} 
+        frames={frames}
+        frameNum={frameNum}
         changeFrame={i => changeFrame(i)}
-        />
+      />
     </div>
-  )
-}
+  );
+};
 
 const useInterval = (callback, delay) => {
   const savedCallback = useRef();
@@ -85,6 +118,6 @@ const useInterval = (callback, delay) => {
       return () => clearInterval(id);
     }
   }, [delay]);
-}
+};
 
-export default Animator
+export default Animator;
